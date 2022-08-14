361ab523502981732da9a64cf65b1e7312a358ba
bugyalu <774933704@qq.com>
2022-08-15 01:17:30 +0800

    init second

--- src/main.tsx
+++ src/main.tsx
@@ -12,11 +12,11 @@ import '~styles/styles.scss';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
+	<>
 		<ErrorBoundary FallbackComponent={ErrorFallback}>
 			<BrowserRouter>
 				<App />
 			</BrowserRouter>
 		</ErrorBoundary>
-	</React.StrictMode>,
+	</>,
 );
