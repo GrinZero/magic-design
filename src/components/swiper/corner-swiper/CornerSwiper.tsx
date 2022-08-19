dfd39092203c977115a470d26b543c9f28fa0904
bugyalu <774933704@qq.com>
2022-08-19 16:33:03 +0800

    feat: corner-swiper 50%

--- src/components/swiper/corner-swiper/CornerSwiper.tsx
+++ src/components/swiper/corner-swiper/CornerSwiper.tsx
@@ -6,13 +6,15 @@ import styles from './CornerSwiper.module.scss';
 interface CornerSwiperProps extends SwiperProps {
 	defaultMargin?: boolean;
 	rotate?: string;
+	position?: number | string;
 }
 
 const CornerSwiper: React.FC<CornerSwiperProps> = ({
 	defaultCurrent = 0,
 	defaultMargin = true,
 	children,
-	rotate = '45deg',
+	position = '20%',
+	rotate = '-45deg',
 	className = '',
 	// autoplay,
 	// duration,
@@ -24,44 +26,45 @@ const CornerSwiper: React.FC<CornerSwiperProps> = ({
 		if (!defaultMargin) {
 			return childList;
 		}
-		return childList.map((child, index) => {
-			const position = 10;
-			const positionStr = `${position}%`;
-			const element3D = React.cloneElement(child, {
+		return childList.map((child) =>
+			React.cloneElement(child, {
 				...child.props,
-				key: `${child.key}-Left`,
-				className: `mg-relative mg-overflow-hidden ${styles['element-3d']} ${child.props.className}`,
-				// eslint-disable-next-line @typescript-eslint/naming-convention
-				style: { '--position': positionStr, '--rotate': rotate },
-			});
-			const element2D = React.cloneElement(child, {
-				...child.props,
-				key: `${child.key}-Right`,
-				className: `mg-absolute mg-overflow-hidden mg-top-0 mg-left-0 ${styles['element-2d']} ${child.props.className}`,
-				// eslint-disable-next-line @typescript-eslint/naming-convention
-				style: { '--position': positionStr },
-			});
-
-			const baseClassName = `${child.props.className || ''} mg-mr-5 mg-relative ${
-				styles['swiper-item']
-			}`;
-			const element = (
-				<div key={child.key || index} className={baseClassName}>
-					{[element3D, element2D]}
-				</div>
-			);
-			return element;
-		});
+				className: `${child.props.className || ''} mg-mr-5`,
+			}),
+		);
 	}, [children, defaultMargin]);
+
+	const swiperElement = (
+		<div
+			className={`mg-w-full mg-h-full mg-flex-row  mg-overflow-hidden mg-flex mg-flex-nowrap ${className}`}
+		>
+			{childrenList}
+		</div>
+	);
+
+	const translateDistance = -100;
+	const element3D = React.cloneElement(swiperElement, {
+		...swiperElement.props,
+		className: `mg-relative ${styles['element-3d']} ${swiperElement.props.className}`,
+		// eslint-disable-next-line @typescript-eslint/naming-convention
+		style: { '--position': position, '--rotate': rotate },
+	});
+	const element2D = React.cloneElement(swiperElement, {
+		...swiperElement.props,
+		className: `mg-absolute mg-top-0 mg-left-0 ${styles['element-2d']} ${swiperElement.props.className}`,
+		// eslint-disable-next-line @typescript-eslint/naming-convention
+		style: { '--position': position },
+	});
 
 	return (
 		<div
-			className={`mg-w-full mg-h-full mg-flex-row mg-flex mg-flex-nowrap mg-relative ${styles.cornerSwiper} ${className}`}
-			style={{
-				transform: `translateX(-${currentIndex * 100}%)`,
-			}}
+			className={`${styles['swiper-container']}`}
+			style={{ transform: `translatex(${translateDistance * currentIndex}px)` }}
 		>
-			{childrenList}
+			<div className={`mg-w-full mg-h-full mg-relative ${styles['corner-swiper']}`}>
+				{element3D}
+				{element2D}
+			</div>
 		</div>
 	);
 };
